89290c9927e7d444dae9fbbc0634e511a73cb429
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-10-20 18:53:25 -0500

    Complete 'Axios To-Do List Part: 1' assignment

--- week4/axiosToDoP1/index.js
+++ week4/axiosToDoP1/index.js
@@ -1,20 +1,29 @@
+let mainDiv = document.querySelector("#mainDiv");
+
+let toDoHeader = document.createElement("h1");
+toDoHeader.textContent = "To-Do List!";
+mainDiv.appendChild(toDoHeader);
+
 axios.get("http://api.bryanuniversity.edu/easton/list")
     .then(res => {
         res.data.forEach((e, i) => {
-            let currToDoEntry = document.createElement("h1");
+            let entryDiv = document.createElement("div");
+            let currToDoEntry = document.createElement("h2");
             let detailList = document.createElement("ul");
             let currDetailEntry = document.createElement("li");
             currToDoEntry.textContent = res.data[i].name;
             currDetailEntry.textContent = res.data[i].description;
+            entryDiv.setAttribute("class", "entry");
             
             if (res.data[i].isComplete == true)
             {
                 currToDoEntry.setAttribute("style", "text-decoration: line-through")
                 currDetailEntry.setAttribute("style", "text-decoration: line-through")
             }
-
-            document.body.appendChild(currToDoEntry);
-            document.body.appendChild(detailList);
+            
+            mainDiv.appendChild(entryDiv);
+            entryDiv.appendChild(currToDoEntry);
+            entryDiv.appendChild(detailList);
             detailList.appendChild(currDetailEntry);
         });
     })
